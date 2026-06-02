13871a05a1ba7070552ceb86858c9485a063b813
SR Rizwan <srizwan198704@gmail.com>
2026-06-02 14:51:38 +0600

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // আধুনিক অ্যান্ড্রয়েড বিল্ডের জন্য স্থিতিশীল ভার্সন
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
